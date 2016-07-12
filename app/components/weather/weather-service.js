73cc9787f1f2f97a11f77ddfc9d78e331ac38128
Jake Overall <joverall22@gmail.com>
2016-07-12 01:00:54 -0600

    resolves merge

--- app/components/weather/weather-service.js
+++ app/components/weather/weather-service.js
@@ -5,11 +5,7 @@ function WeatherService(){
 		if(w){
 			w = JSON.parse(w);
 			console.log('Weather Data:', w)
-<<<<<<< HEAD
-			return callWhenDone(w);
-=======
 			return callWhenDone(w)
->>>>>>> ccf75870746f3593cab7db38eb273d77b4d21c5f
 		}
 		
 		$.get('http://api.openweathermap.org/data/2.5/weather?q=boise&&APPID=bd82255fd0a21fa1238699b9eda2ee35', function(res){
