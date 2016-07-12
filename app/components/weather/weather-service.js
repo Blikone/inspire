eb1b6852e049a53e3c21f201946208f657327bb4
Jake Overall <joverall22@gmail.com>
2016-07-12 09:50:37 -0600

    fixes weather service using bcw-getter

--- app/components/weather/weather-service.js
+++ app/components/weather/weather-service.js
@@ -1,7 +1,7 @@
 function WeatherService(){
 	
 	var url = '//bcw-getter.herokuapp.com/?url=';
-	var url2 = '//api.openweathermap.org/data/2.5/weather?q=boise&&APPID=bd82255fd0a21fa1238699b9eda2ee35';
+	var url2 = 'http://api.openweathermap.org/data/2.5/weather?q=boise&&APPID=bd82255fd0a21fa1238699b9eda2ee35';
 	var apiUrl = url + encodeURIComponent(url2);
 
 	this.getWeather =  function(callWhenDone){
@@ -14,7 +14,8 @@ function WeatherService(){
 		
 		$.get(apiUrl, function(res){
 			localStorage.setItem('weather', JSON.stringify(res))
-			callWhenDone(res);
+			debugger
+			callWhenDone(JSON.parse(res));
 		})
 	}
 }
