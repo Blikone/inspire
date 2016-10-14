8d8abb4254d24b09b876ace897ae3e9a6eeffa49
Jonathan Steenblik <blikone@hotmail.com>
2016-10-14 17:01:11 -0600

    Set up a clock

--- app/init.js
+++ app/init.js
@@ -8,4 +8,40 @@ function namePrompt() {
         document.getElementById('first-name').innerText = firstName;
     }
 }
+function startTime() {
+   	var today = new Date();
+   	var h = today.getHours();
+	var m = today.getMinutes();
+	// var s = today.getSeconds();
+	m = checkTime(m);
+	// s = checkTime(s);
+	document.getElementById('current-time').innerHTML = h + ":" + m;
+	var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+	if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+function setGreeting() {
+    var today = new Date();
+    var h = today.getHours();
+    var greeting
+    switch(h) {
+        case 5,6,7,8,9,10,11:
+            greeting = "Good morning, ";
+            break;
+        case 12,13,14,15,16:
+            greeting = "Good afternoon, ";
+            break;
+        case 17,18,19,20,21:
+            greeting = "Good evening, ";
+            break;
+        default:
+            greeting = "Go to bed, ";
+    }
+    document.getElementById('greeting').innerHTML = greeting;
+}
+
 namePrompt();
+startTime();
+setGreeting();
