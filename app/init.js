3ef1706ccf3eaf7665d404df98c9ecded4cea114
Jonathan Steenblik <blikone@hotmail.com>
2016-10-14 16:25:53 -0600

    User name input with center display

--- app/init.js
+++ app/init.js
@@ -1,1 +1,11 @@
-
+function namePrompt() {
+    var userName = prompt("Please enter your name", "Spike Spiegel");
+    console.log(userName);
+    var firstName = userName.split(' ')[0];
+    console.log(firstName);
+    if (firstName) {
+        console.log("Hello, " + firstName)
+        document.getElementById('first-name').innerText = firstName;
+    }
+}
+namePrompt();
