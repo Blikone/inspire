260b281f86e97c541edf0c7ae40a1a7e8609d85d
Jonathan Steenblik <blikone@hotmail.com>
2016-10-14 12:05:01 -0600

    Temperature toggle added

--- app/components/image/image-controller.js
+++ app/components/image/image-controller.js
@@ -2,6 +2,6 @@
 	var imageService = new ImageService();
 	imageService.getImage(setBackground)
 	function setBackground(image) {
-		$('body').css('background-image', `url("${image.url}")`);
+		$('body').css('background-image', `url("${image.url}")`).css('background-size', 'cover');
 	}	
 }())
--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -11,10 +11,9 @@
 	})
 	updateWeather = function() {
 		var tempC = wc.weather.main.temp - 273.15;
-		var tempF = tempC * (9/5) + 32;
 		var template = `
 			<div class="temp">
-				<p>${Math.round(tempC)}&deg;C</p>
+				<button class="celsius" id="temp-click">${Math.round(tempC)}&deg;C</button>
 			</div>
 			<div class="description">
 				<p>${wc.weather.weather[0].description}</p>
@@ -24,6 +23,26 @@
 	}
 	updateWeather();
 	
+	$('#weather').on('click', '#temp-click', function() {
+		var tempK = wc.weather.main.temp
+		var tempC = wc.weather.main.temp - 273.15;
+		var tempF = tempC * (9/5) + 32;
+		var degree = $('#temp-click').attr('class');
+		switch(degree) {
+			case "celsius":
+				$('#temp-click').html(`${Math.round(tempF)}&deg;F`);
+				$('#temp-click').addClass('fahrenheit').removeClass('celsius');
+				break;
+			case "fahrenheit":
+				$('#temp-click').html(`${Math.round(tempK)}&deg;K`);
+				$('#temp-click').addClass('kelvin').removeClass('fahrenheit');
+				break;
+			case "kelvin":
+				$('#temp-click').html(`${Math.round(tempC)}&deg;C`);
+				$('#temp-click').addClass('celsius').removeClass('kelvin');
+				break;
+		}
+	})
 	
 	
 	
