d0a9d8a15e580b4e8de46cbd30174ae8a7dacefc
Jonathan Steenblik <blikone@hotmail.com>
2016-10-14 10:29:30 -0600

    Quote writes to screen

--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -1,5 +1,12 @@
 (function(){
-	
-	//Get your QuoteService
+	quoteService = new QuoteService();
+	quoteService.getQuote(function(quote) {
+		var template = `
+			<div class="quote-well">
+				<p>${quote.quote} <span id="author"><small>${quote.author}</small></span></p>
+			</div>
+		`;
+		$('#quote').html(template);
+	});
 	
 }())
--- app/components/quote/quote-service.js
+++ app/components/quote/quote-service.js
@@ -7,7 +7,8 @@ function QuoteService(){
 	this.getQuote =  function(callWhenDone){
 		$.get(apiUrl, function(res){
 			console.log('Quote Data:', res)
-			//Now What?
+			var quoteObj = JSON.parse(res);
+			callWhenDone(quoteObj);
 		})
 	}
 }
