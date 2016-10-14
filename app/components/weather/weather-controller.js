cba488b39f57b6a6a8cbef11f2904afd29df120b
Jonathan Steenblik <blikone@hotmail.com>
2016-10-14 10:12:35 -0600

    Writing weather info to page

--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -2,12 +2,27 @@
 	
 	var wc = this;
 	var weatherService = new WeatherService();
+	wc.weather;
 	
 	weatherService.getWeather(function(weather){
-		console.log(weather);
-		//What can you do with this weather object?
+		// console.log(typeof(weather));
+		wc.weather = JSON.parse(weather);
+		// console.log(typeof(wc.weather));
 	})
-	
+	updateWeather = function() {
+		var tempC = wc.weather.main.temp - 273.15;
+		var tempF = tempC * (9/5) + 32;
+		var template = `
+			<div class="temp">
+				<p>${Math.round(tempC)}&deg;C</p>
+			</div>
+			<div class="description">
+				<p>${wc.weather.weather[0].description}</p>
+			</div>
+		`;
+		$('#weather').html(template);
+	}
+	updateWeather();
 	
 	
 	
