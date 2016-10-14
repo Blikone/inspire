1cfe0f2cb23cbd00f4ea57d1554c06eeba20c7a3
Jonathan Steenblik <blikone@hotmail.com>
2016-10-14 11:36:17 -0600

    Todo list and add function working

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -1,8 +1,33 @@
 (function(){
-	// new up the TodoService that has already been configured for your use
+	var todoService = new TodoService();
+	var todos = [];
+
+	function updateTodos() {
+		$('#todo-list').html('');
+		todos = todoService.getTodos();
+		todos.forEach(function(item) {
+			$('#todo-list').append(`<li>${item}</li>`)
+		});
+	}
+
+	updateTodos(todoService.getTodos());
+
+	function saveList(list) {
+		todoService.saveTodos(list);
+	}
+
+	$('.add-to-list').on('submit', function(event) {
+		event.preventDefault();
+		var form = event.target;
+		// debugger;
+		todos.push(form.item.value);
+		saveList(todos);
+		updateTodos(todoService.getTodos());
+		form.item.value = '';
+	})
+	
 	// There are two methods getTodos returns and array
 	// saveTodos accepts an array and stores it to your local storage
-	
 	
 	
 	
